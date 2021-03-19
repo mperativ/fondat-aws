b1fa3f5e152a3603df93680b1d268d2134f0a182
tguo9 <taoguoubc@gmail.com>
2021-03-18 22:02:31 -0700

    add code structure

--- fondat/aws/cloudwatch.py
+++ fondat/aws/cloudwatch.py
@@ -46,4 +46,28 @@ def cloudwatch_resource(
             Namespace = measurement
             )
 
-    return response
+            return response
+
+        @cloudwatch_resource
+        async def put_alarm(self, measurement: Measurement) :
+            response = client.put_metric_alarm(
+            AlarmName='Web_Server_CPU_Utilization',
+            ComparisonOperator='GreaterThanThreshold',
+            EvaluationPeriods=1,
+            MetricName='CPUUtilization',
+            Namespace='AWS/EC2',
+            Period=60,
+            Statistic='Average',
+            Threshold=70.0,
+            ActionsEnabled=False,
+            AlarmDescription='Alarm when server CPU exceeds 70%',
+            Dimensions=[
+                {
+                'Name': 'InstanceId',
+                'Value': 'INSTANCE_ID'
+                },
+            ],
+            Unit='Seconds'
+            )
+
+            return response
