4e0cf657ca62d61cbad094002b5b86fa7d6a504e
Paul Bryan <pbryan@anode.ca>
2021-06-07 14:34:40 -0700

    Better teardown of verified identity in test fixture.

--- tests/test_ses.py
+++ tests/test_ses.py
@@ -18,6 +18,9 @@ config = Config(
 )
 
 
+verified_source = "source@test.io"
+
+
 @pytest.fixture(scope="module")
 def event_loop():
     loop = asyncio.new_event_loop()
@@ -38,11 +41,13 @@ async def resource(client):
 
 @pytest.fixture(scope="module", autouse=True)
 async def setup_module(resource):
-    await resource.identities.post("source@test.io")
+    await resource.identities.post(verified_source)
+    yield
+    await resource.identities[verified_source].delete()
 
 
 async def test_send_raw_verified_identity(resource, setup_module):
-    await resource.send_raw_email("source@test.io", "destination@test.io", b"message")
+    await resource.send_raw_email(verified_source, "destination@test.io", b"message")
 
 
 async def test_send_unverified_identity(resource):
